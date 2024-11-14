7c9ffb4c63a12e86944426fc9e9dc51430cfa6fa
yqlai <jeffery260017@gmail.com>
2024-11-14 14:23:34 +0800

    1114

--- scripts/CP.py
+++ scripts/CP.py
@@ -9,6 +9,10 @@ class Content_Provider:
         self.user_request_queue = [0] * (self.serve_offset+1)
         self.age = 0
 
+        self.update_rate_upper_bound = config.config['update_rate_upper_bound']
+        self.num_update = 0
+        self.num_time_slot = 0
+
         # Arrival rate = (id ^ -kappa) / sum(i ^ -kappa)
         self.arrival_rate = (id ** -config.config['kappa']) / sum([i ** -config.config['kappa'] for i in range(1, config.config['num_content']+1)])
         self.arrival_rate = 0.6
@@ -22,16 +26,20 @@ class Content_Provider:
         for i in range(self.serve_offset+1):
             self.user_request_queue[i] = self.new_arrive()
         self.age = 1
+        self.num_update = 0
+        self.num_time_slot = 0
     
     # Move the user request[1:delta] to [0:delta-1] and add new user request at the end
     # Add age according to the update flag
     # Return the age and the number of user requests
     def step(self, update_id):
+        self.num_time_slot += 1
         current_state = [self.age] + self.user_request_queue
         age, expired_user_request = self.age, self.user_request_queue[0]
         if update_id == self.id:
             update_indicator = 1
             self.age = 1
+            self.num_update += 1
         else:
             update_indicator = 0
             self.age += 1
@@ -45,4 +53,4 @@ class Content_Provider:
         return np.random.binomial(self.num_user, self.arrival_rate)
 
     def Lagrangian(self, age, user_request, update_indicator):
-        return (age * user_request) / self.num_user * self.arrival_rate + self.eta * (update_indicator)
+        return (age * user_request) / self.num_user * self.arrival_rate + self.eta * max(update_indicator - self.update_rate_upper_bound, 0)
--- scripts/config.py
+++ scripts/config.py
@@ -1,3 +1,4 @@
+import time
 class Config:
     def __init__(self):
         self.config = {
@@ -7,16 +8,19 @@ class Config:
             'num_user': 3,
             'serve_offset': 6,
             'num_time_slot_in_episode': 1000,
-            'num_episode': 200,
+            'num_episode': 15,
             'ref_state': [2, 1, 0, 1, 0, 1, 0, 1],
+            'update_rate_upper_bound': 0.05,
 
             # Arrival parameters
             'kappa': 1.5,
 
             # DDPG
             'warmup':  200,
-            'eta': 50,
+            'eta': 15,
 
             # Output
             'output_file': 'eta50.png'
         }
+        # filename = 'MMddhhmm_{eta}_{serve_offset}_{num_content}.png'
+        self.config['output_file'] = 'Result/' + time.strftime('%m%d%H%M') + '_' + str(self.config['eta']) + '_' + str(self.config['serve_offset']) + '_' + str(self.config['num_content']) + '.png'
